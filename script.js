5d7381e9435b7d000c92c14aeaa1f2c968fd89d0
Fozilxoja <fozilxoja@gmail.com>
2022-04-03 12:26:15 +0500

    daraja

---
+++ script.js
@@ -0,0 +1,9 @@
+let num =prompt('son kiriting')
+let doraja =prompt('darajani kiriting')
+let jovob = 1
+
+for(let i = 0 ; i < doraja; i++ ){
+    jovob = jovob * num
+}
+// console.log(jovob);
+let natija =alert(`javob  ${jovob}`)
